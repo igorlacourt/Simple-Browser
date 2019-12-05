4db91323f27e08020e0cd580332b99973f03758f
globalsys.igor <globalsys.igor@meupag.com.br>
2019-12-05 10:56:46 -0300

    checkHostName function created.

--- app/src/main/java/com/lacourt/dynamiclink/MainActivity.kt
+++ app/src/main/java/com/lacourt/dynamiclink/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.lacourt.dynamiclink
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,12 +8,18 @@ import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
+import androidx.core.content.ContextCompat.startActivity
+import android.content.Intent
+import androidx.core.content.ContextCompat
 
-class MainActivity : AppCompatActivity(){//, View.OnKeyListener {
+
+class MainActivity : AppCompatActivity() {
+    //, View.OnKeyListener {
     var siteEditText: EditText? = null
     var myWebView: WebView? = null
     var site: String? = null
@@ -23,19 +30,12 @@ class MainActivity : AppCompatActivity(){//, View.OnKeyListener {
 
         siteEditText = findViewById<EditText>(R.id.site_path)
         myWebView = findViewById(R.id.webview)
-        site = "https://www.meupag.com.br"
+        myWebView?.webViewClient = WebViewClient()
+        site = "https://meupag.com.br"
 
 //        event.action == KeyEvent.ACTION_DOWN &&
 
-        siteEditText?.setOnKeyListener { v, keyCode, event ->
-            if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                Log.d("link-log", "Enter Key Pressed!")
-                loadPage(siteEditText?.text.toString())
-                true
-            }
 
-            false
-        }
 
         FirebaseDynamicLinks.getInstance()
             .getDynamicLink(intent)
@@ -65,31 +65,60 @@ class MainActivity : AppCompatActivity(){//, View.OnKeyListener {
                 Log.d("link-log", "getDynamicLink:onFailure", e)
             }
 
+        siteEditText?.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                Log.d("link-log", "Enter Key Pressed!")
+                loadPage(siteEditText?.text.toString())
+                true
+            }
+
+            false
+        }
+
     }
 
-//    override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
-//        if (event != null) {
-//            if (keyCode == EditorInfo.IME_ACTION_SEARCH ||
-//                keyCode == EditorInfo.IME_ACTION_DONE ||
-//                event.getAction() == KeyEvent.ACTION_DOWN &&
-//                event.getKeyCode() == KeyEvent.KEYCODE_ENTER
-//            ) {
-//                Log.v("link-log", "on key event called")
-//                if (!event.isShiftPressed()) {
-//                    Log.v("link-log", "Enter Key Pressed!")
-//                    if (v?.getId() == R.id.site_path) {
-//                        loadPage(siteEditText?.text.toString())
-//                    }
-//                    return true
-//                }
-//
-//            }
-//        }
-//        return false
-//    }
+    private fun checkHost(url: String){
+        Log.d("link-log", "url = $url")
+        Log.d("link-log", "Uri.parse(url).host = $url")
+        checkHostName(url)
 
-    private fun loadPage(link: String) {
-        Log.d("link-log", "loadPage called, link = $link")
-        myWebView?.loadUrl(link)
+    }
+
+    private fun loadPage(url: String) {
+        Log.d("link-log", "loadPage called, link = $url")
+
+        if (url.contains("meupag")) {
+            Log.d("link-log", "inside if")
+            myWebView?.loadUrl(checkHostName(url))
+        } else {
+//            val intent = Intent(Intent.ACTION_VIEW)
+//            intent.data = Uri.parse(url)
+//            val chooser = Intent.createChooser(intent, "Escolha um navegador")
+//            startActivity(this, chooser, null)
+
+            Log.d("link-log", "inside else")
+//            Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+                startActivity(this)
+            }
+        }
+    }
+
+    private fun checkHostName(url: String): String{
+        var prefix = "http://"
+        var lowerCaseUrl = url.toLowerCase()
+        var result = ""
+        if(lowerCaseUrl.contains("meupag.com.br")) {
+            if(!lowerCaseUrl.contains(prefix))
+                result = prefix + lowerCaseUrl
+        }
+        return if(result.subSequence(0, 20).contains("http://meupag.com.br")) {
+            Log.d("link-log", "url substring = ${result.subSequence(0, 20)}" )
+            result
+        }
+        else
+            "http://meupag.com.br/parceiros"
+
     }
 }
+
