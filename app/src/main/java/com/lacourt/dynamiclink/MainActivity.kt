332a664ca4d5be66c155e519265e9be818d7386f
globalsys.igor <globalsys.igor@meupag.com.br>
2019-12-05 12:22:36 -0300

    Back Navigation implemented

--- app/src/main/java/com/lacourt/dynamiclink/MainActivity.kt
+++ app/src/main/java/com/lacourt/dynamiclink/MainActivity.kt
@@ -7,15 +7,16 @@ import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import androidx.core.content.ContextCompat.startActivity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.http.SslError
+import android.os.Message
+import android.webkit.*
+import android.widget.*
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,14 +24,192 @@ class MainActivity : AppCompatActivity() {
     var siteEditText: EditText? = null
     var myWebView: WebView? = null
     var site: String? = null
+    var loadingWebPage: ProgressBar? = null
+    var webError: TextView? = null
+    var url: String = "meu pag"
+    var backButton: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        siteEditText = findViewById<EditText>(R.id.site_path)
+        backButton = findViewById(R.id.btn_arrow)
+        loadingWebPage = findViewById(R.id.loading_web_page)
+        siteEditText = findViewById(R.id.site_path)
         myWebView = findViewById(R.id.webview)
-        myWebView?.webViewClient = WebViewClient()
+        myWebView?.settings?.javaScriptEnabled = true
+
+        myWebView?.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+
+                loadingWebPage?.visibility = View.INVISIBLE
+            }
+
+            override fun shouldInterceptRequest(
+                view: WebView?,
+                url: String?
+            ): WebResourceResponse? {
+//                siteEditText?.setText(url)
+                return super.shouldInterceptRequest(view, url)
+            }
+
+            override fun shouldInterceptRequest(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): WebResourceResponse? {
+//                siteEditText?.setText(url)
+                return super.shouldInterceptRequest(view, request)
+            }
+
+            override fun shouldOverrideKeyEvent(view: WebView?, event: KeyEvent?): Boolean {
+                return super.shouldOverrideKeyEvent(view, event)
+            }
+
+            override fun onSafeBrowsingHit(
+                view: WebView?,
+                request: WebResourceRequest?,
+                threatType: Int,
+                callback: SafeBrowsingResponse?
+            ) {
+                super.onSafeBrowsingHit(view, request, threatType, callback)
+            }
+
+            override fun hashCode(): Int {
+                return super.hashCode()
+            }
+
+            override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
+                super.doUpdateVisitedHistory(view, url, isReload)
+            }
+
+            override fun onReceivedError(
+                view: WebView?,
+                errorCode: Int,
+                description: String?,
+                failingUrl: String?
+            ) {
+
+            }
+
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+//                super.onReceivedError(view, request, error)
+//                if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M){
+//                    Log.d("web-error", "API greater than 23")
+//                    Log.d("web-error", error?.errorCode.toString())
+//                    Log.d("web-error", error?.description.toString())
+//
+//                    webError?.text = error.toString()
+//                }
+                myWebView?.loadUrl("https://www.google.com/search?q=${siteEditText?.text}")
+            }
+
+            override fun onRenderProcessGone(
+                view: WebView?,
+                detail: RenderProcessGoneDetail?
+            ): Boolean {
+                return super.onRenderProcessGone(view, detail)
+            }
+
+            override fun onReceivedLoginRequest(
+                view: WebView?,
+                realm: String?,
+                account: String?,
+                args: String?
+            ) {
+                super.onReceivedLoginRequest(view, realm, account, args)
+            }
+
+            override fun equals(other: Any?): Boolean {
+                return super.equals(other)
+            }
+
+            override fun toString(): String {
+                return super.toString()
+            }
+
+            override fun onReceivedHttpError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                errorResponse: WebResourceResponse?
+            ) {
+                super.onReceivedHttpError(view, request, errorResponse)
+
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                loadingWebPage?.visibility = View.VISIBLE
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onScaleChanged(view: WebView?, oldScale: Float, newScale: Float) {
+                super.onScaleChanged(view, oldScale, newScale)
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                return super.shouldOverrideUrlLoading(view, url)
+            }
+
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageCommitVisible(view: WebView?, url: String?) {
+                super.onPageCommitVisible(view, url)
+            }
+
+            override fun onUnhandledKeyEvent(view: WebView?, event: KeyEvent?) {
+                super.onUnhandledKeyEvent(view, event)
+            }
+
+            override fun onReceivedClientCertRequest(view: WebView?, request: ClientCertRequest?) {
+                super.onReceivedClientCertRequest(view, request)
+            }
+
+            override fun onReceivedHttpAuthRequest(
+                view: WebView?,
+                handler: HttpAuthHandler?,
+                host: String?,
+                realm: String?
+            ) {
+                super.onReceivedHttpAuthRequest(view, handler, host, realm)
+            }
+
+            override fun onReceivedSslError(
+                view: WebView?,
+                handler: SslErrorHandler?,
+                error: SslError?
+            ) {
+                super.onReceivedSslError(view, handler, error)
+            }
+
+            override fun onTooManyRedirects(
+                view: WebView?,
+                cancelMsg: Message?,
+                continueMsg: Message?
+            ) {
+                super.onTooManyRedirects(view, cancelMsg, continueMsg)
+            }
+
+            override fun onFormResubmission(
+                view: WebView?,
+                dontResend: Message?,
+                resend: Message?
+            ) {
+                super.onFormResubmission(view, dontResend, resend)
+            }
+
+            override fun onLoadResource(view: WebView?, url: String?) {
+                super.onLoadResource(view, url)
+            }
+        }
         site = "https://meupag.com.br"
 
 //        event.action == KeyEvent.ACTION_DOWN &&
@@ -65,16 +244,40 @@ class MainActivity : AppCompatActivity() {
                 Log.d("link-log", "getDynamicLink:onFailure", e)
             }
 
+        backButton?.setOnClickListener {
+            navigateBack()
+        }
+
         siteEditText?.setOnKeyListener { v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER) {
                 Log.d("link-log", "Enter Key Pressed!")
-                loadPage(siteEditText?.text.toString())
+//                loadPage(siteEditText?.text.toString())
+                myWebView?.loadUrl(formatUrl(siteEditText?.text.toString()))
                 true
             }
 
             false
         }
 
+    }
+
+    private fun navigateBack() {
+        if (myWebView?.copyBackForwardList() != null){
+            if (myWebView!!.copyBackForwardList().currentIndex > 0) {
+                myWebView!!.goBack()
+            } else {
+                // Your exit alert code, or alternatively line below to finish
+                super.onBackPressed()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        navigateBack()
+    }
+
+    private fun formatUrl(url: String): String? {
+        return "http://$url"
     }
 
     private fun checkHost(url: String){
@@ -91,13 +294,7 @@ class MainActivity : AppCompatActivity() {
             Log.d("link-log", "inside if")
             myWebView?.loadUrl(checkHostName(url))
         } else {
-//            val intent = Intent(Intent.ACTION_VIEW)
-//            intent.data = Uri.parse(url)
-//            val chooser = Intent.createChooser(intent, "Escolha um navegador")
-//            startActivity(this, chooser, null)
-
             Log.d("link-log", "inside else")
-//            Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
             Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
                 startActivity(this)
             }
@@ -118,7 +315,6 @@ class MainActivity : AppCompatActivity() {
         }
         else
             "http://meupag.com.br/parceiros"
-
     }
 }
 
